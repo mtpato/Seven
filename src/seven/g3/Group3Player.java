64bb486d69a963847b60d038e362d34ccd8b9460
Michael Pato <mtpato@gmail.com>
2012-06-07 12:34:11 -0400

    starting repo

---
+++ src/seven/g3/Group3Player.java
@@ -0,0 +1,217 @@
+package seven.g3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import seven.ui.Letter;
+import seven.ui.LetterGame;
+import seven.ui.Player;
+import seven.ui.PlayerBids;
+import seven.ui.SecretState;
+
+public class Group3Player implements Player {
+
+	/*
+	 * @SuppressWarnings("serial") /private class CharBag extends
+	 * ArrayList<Character> { public CharBag(String word) { for (char c :
+	 * word.toCharArray()) this.add(c); }
+	 * 
+	 * public CharBag(CharBag c) { super(c); }
+	 * 
+	 * public CharBag() { }
+	 * 
+	 * @Override public boolean equals(Object obj) { if (obj instanceof CharBag)
+	 * return equals((CharBag) obj);
+	 * 
+	 * return false; }
+	 * 
+	 * @Override public int hashCode() { Collections.sort(this); return
+	 * super.hashCode(); }
+	 * 
+	 * public boolean equals(CharBag c) { Collections.sort(this);
+	 * Collections.sort(c); return super.equals(c); } }
+	 */
+
+	/*
+	 * private class MakeSeven { public boolean yes; public int count; public
+	 * List<Character> characters;
+	 * 
+	 * public MakeSeven(boolean yes, int count, List<Character> characters) {
+	 * this.yes = yes; this.count = count; this.characters = characters; } }
+	 */
+
+	private final Logger log = Logger.getLogger(this.getClass());
+
+	// private final Set<CharBag> sevenBags = new HashSet<CharBag>();
+	// private CharBag myBag = new CharBag();
+
+	// private List<Word> sevenList = new ArrayList<Word>();
+
+	private List<Set<Character>> makeSeven = new ArrayList<Set<Character>>();
+	private int myID;
+	private List<Character> currentLetters;
+	private List<Word> wordlist = new ArrayList<Word>();
+
+	{
+		try {
+			BufferedReader r = new BufferedReader(new FileReader("textFiles/dictionary.txt"));
+			String line = r.readLine(); // skip first line
+
+			while (null != (line = r.readLine())) {
+				line = line.trim();
+				line = line.substring(line.indexOf(',') + 1);
+
+				wordlist.add(new Word(line));
+
+				if (line.length() == 7) {
+					Set<Character> row = new HashSet<Character>();
+
+					for (char c : line.toCharArray())
+						row.add(c);
+
+					makeSeven.add(row);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// private List<Character> myBag = new ArrayList<Character>();
+
+	/*
+	 * private class MakeSeven { public int count; public Word w;
+	 * 
+	 * public MakeSeven(int count, Word w) { this.count = count; this.w = w; } }
+	 * 
+	 * private class BettingTable { public Map<Character, Integer> map = new
+	 * HashMap<Character, Integer>(); public int max = Integer.MIN_VALUE; }
+	 * 
+	 * private BettingTable buildBettingTable(List<Character> bagList,
+	 * List<Character> remainingChars) { BettingTable tbl = new BettingTable();
+	 * String bag = new String();
+	 * 
+	 * for (Character c : bagList) bag += c;
+	 * 
+	 * for (Character c : remainingChars) { List<Character> r = new
+	 * ArrayList<Character>(remainingChars); r.remove(c);
+	 * 
+	 * MakeSeven s = makeSeven(new Word(bag + c), r, 0);
+	 * 
+	 * if (s.count > tbl.max) tbl.max = s.count;
+	 * 
+	 * tbl.map.put(c, s.count); }
+	 * 
+	 * log.trace("Done!");
+	 * 
+	 * return tbl; }
+	 * 
+	 * private MakeSeven makeSeven(Word bag, List<Character> rest, int depth) {
+	 * if (depth == 1) return new MakeSeven(0, null);
+	 * 
+	 * for (Word w : sevenList) if (bag.contains(w)) return new MakeSeven(1, w);
+	 * 
+	 * MakeSeven ret = new MakeSeven(0, null);
+	 * 
+	 * for (Character c : rest) { List<Character> r = new
+	 * ArrayList<Character>(rest); r.remove(c);
+	 * 
+	 * MakeSeven s = makeSeven(new Word(bag.word + c), r, depth + 1);
+	 * 
+	 * if (s.count > 0 && s.w.word.indexOf(c) > -1) {// fix this later ret.count
+	 * += s.count; ret.w = s.w; } }
+	 * 
+	 * return ret; }
+	 */
+
+	@Override
+	public void newGame(int id, int number_of_rounds, int number_of_players) {
+		myID = id;
+	}
+
+	@Override
+	public void newRound(SecretState secretState, int current_round) {
+		currentLetters = new ArrayList<Character>();
+
+		for (Letter l : secretState.getSecretLetters())
+			currentLetters.add(l.getCharacter());
+	}
+
+	@Override
+	public int getBid(Letter bidLetter, ArrayList<PlayerBids> PlayerBidList, ArrayList<String> PlayerList, SecretState secretstate) {
+		Character c = bidLetter.getCharacter();
+		int maxSize = Integer.MIN_VALUE;
+		int collides = 0;
+
+		for (Set<Character> set : makeSeven)
+			if (set.contains(c)) {
+				maxSize = set.size() > maxSize ? set.size() : maxSize;
+				collides++;
+			}
+
+		int bet = (int) (((double) collides / makeSeven.size()) * (secretstate.getScore() / 2));
+
+		Random r = new Random();
+
+		return bet == 0 ? r.nextInt(secretstate.getScore() / 2) : bet;
+	}
+
+	@Override
+	public void bidResult(boolean won, Letter letter, PlayerBids bids) {
+		if (won) {
+			List<Set<Character>> newSet = new ArrayList<Set<Character>>();
+			Character c = letter.getCharacter();
+
+			for (Set<Character> set : makeSeven)
+				if (set.contains(c)) {
+					set.remove(c);
+					newSet.add(set);
+				}
+
+			makeSeven = newSet;
+			currentLetters.add(letter.getCharacter());
+		}
+	}
+
+	@Override
+	public String getWord() {
+		char c[] = new char[currentLetters.size()];
+		for (int i = 0; i < c.length; i++) {
+			c[i] = currentLetters.get(i);
+		}
+		String s = new String(c);
+		Word ourletters = new Word(s);
+		Word bestword = new Word("");
+		for (Word w : wordlist) {
+			if (ourletters.contains(w)) {
+				if (w.score > bestword.score) {
+					bestword = w;
+				}
+
+			}
+		}
+
+		log.trace("My ID is " + myID + " and my word is " + bestword.word);
+
+		return bestword.word;
+	}
+
+	@Override
+	public void updateScores(ArrayList<Integer> scores) {
+	}
+
+}
---
+++ src/seven/g3/g3player.java
@@ -0,0 +1,249 @@
+package seven.g3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.io.*;
+
+import org.apache.log4j.Logger;
+
+import seven.ui.Letter;
+import seven.ui.LetterGame;
+import seven.ui.Player;
+import seven.ui.PlayerBids;
+import seven.ui.SecretState;
+
+public class g3player implements Player {
+
+	
+	/*
+	 * This player bids randomly.
+	 */
+
+	// an array of words to be used for making decisions
+	private static final Word[] wordlist;
+
+	// for logging
+	private Logger log = Logger.getLogger(this.getClass());
+
+	// the set of letters that this player currently has
+	private ArrayList<Character> currentLetters;
+	
+	// unique ID
+	private int myID;
+	
+	// for generating random numbers
+	private Random random = new Random();
+	
+	private Map<String, Integer> goalWords; 
+		
+	
+	/* This code initializes the word list */
+	static {
+		BufferedReader r;
+		String line = null;
+		ArrayList<Word> wtmp = new ArrayList<Word>(55000);
+		try {
+			// you can use textFiles/dictionary.txt if you want the whole list
+			r = new BufferedReader(new FileReader("textFiles/super-small-wordlist.txt"));
+			while (null != (line = r.readLine())) {
+				wtmp.add(new Word(line.trim()));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		wordlist = wtmp.toArray(new Word[wtmp.size()]);
+	}
+
+	
+    /*
+     * This is called once at the beginning of a Game.
+     * The id is what the game considers to be your unique identifier
+     * The number_of_rounds is the total number of rounds to be played in this game
+     * The number_of_players is, well, the number of players.
+     */
+	public void newGame(int id, int number_of_rounds, int number_of_players) {
+		myID = id;
+		goalWords = new HashMap<String,Integer>();
+		fillHash();
+		
+	}
+	
+	private Map<Character, Integer> count;
+	
+	private int genCounts(List<Character> ours) {
+		List<Letter> bag = LetterGame.getRemainingLetters();
+		
+		if (ours.size() == 7) //&& makes 7 letter word
+			return 1;
+		
+		for (Letter c : bag) {
+			List<Character> l = new ArrayList<Character>(ours);
+			l.add(c.getCharacter());
+			
+			count.put(c.getCharacter(), genCounts(l));		
+		}
+		
+		return 0;
+	}
+
+
+	private void fillHash() {
+		BufferedReader r;
+		String line = null;
+		ArrayList<Word> wtmp = new ArrayList<Word>(55000);
+		try {
+			// you can use textFiles/dictionary.txt if you want the whole list
+			r = new BufferedReader(new FileReader("textFiles/7letterWords.txt"));
+			while (null != (line = r.readLine())) {
+				
+				
+				line = sort(line);
+				Integer i = goalWords.get(line);
+				
+				if(goalWords.containsKey(line)) {
+					 i = new Integer(i + 1);
+				}	
+				
+				goalWords.put(line, i);			
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	private String sort(String line) {
+		
+		char[] list = line.toCharArray();
+		//Collections.sort(list);
+		Arrays.sort(list, 0, list.length);
+		
+		return String.valueOf(list);
+	}
+
+
+	/*
+	 * This method is called at the beginning of a new round.
+	 * The secretState contains your current score and the letters that were secretly given to you in this round
+	 * The current_round indicates the current round number (0-based)
+	 */
+	public void newRound(SecretState secretState, int current_round) {
+
+		// be sure to reinitialize the list at the start of the round
+		currentLetters = new ArrayList<Character>();
+		
+		// add any letters from the secret state
+		for (Letter l : secretState.getSecretLetters()) {
+			//logger.trace("myID = " + myID + " and I'm adding " + l + " from the secret state");
+			currentLetters.add(l.getCharacter());
+		}
+	}
+	
+
+	/*
+	 * This method is called when there is a new letter available for bidding.
+	 * bidLetter = the Letter that is being bid on
+	 * playerBidList = the list of all previous bids from all players
+	 * playerList = the class names of the different players
+	 * secretState = your secret state (which includes the score)
+	 */
+	public int getBid(Letter bidLetter, ArrayList<PlayerBids> playerBidList, ArrayList<String> playerList, SecretState secretState) {
+		
+		wantLetter(bidLetter);
+		//logger.trace("myID=" + myID + " and I'm bidding on " + bidLetter);
+		//logger.trace("myID= " + myID + " and my score is " + secretState.getScore());
+
+		// randomly bid up to half of the remaining points
+		return random.nextInt(secretState.getScore()/2);
+	}
+
+	
+	private boolean wantLetter(Letter bidLetter) {
+		
+		
+		int i = getLetterScore(bidLetter);
+		log.trace(i);
+		return false;
+		
+	}
+
+
+	private int getLetterScore(Letter bidLetter) {
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	/*
+	 * This method is called after a bid. It indicates whether or not the player
+	 * won the bid and what letter was being bid on, and also includes all the
+	 * other players' bids. 
+	 */
+    public void bidResult(boolean won, Letter letter, PlayerBids bids) {
+    	if (won) {
+    		//logger.trace("My ID is " + myID + " and I won the bid for " + letter);
+    		currentLetters.add(letter.getCharacter());
+    	}
+    	else {
+    		//logger.trace("My ID is " + myID + " and I lost the bid for " + letter);
+    	}
+    }
+
+    /*
+     * This method is called after all the letters have been purchased in the round.
+     * The word that you return will be scored for this round.
+     */
+	public String getWord() {
+		char c[] = new char[currentLetters.size()];
+		for (int i = 0; i < c.length; i++) {
+			c[i] = currentLetters.get(i);
+		}
+		String s = new String(c);
+		Word ourletters = new Word(s);
+		Word bestword = new Word("");
+		for (Word w : wordlist) {
+			if (ourletters.contains(w)) {
+				if (w.score > bestword.score) {
+					bestword = w;
+				}
+
+			}
+		}
+		log.trace("My ID is " + myID + " and my word is " + bestword.word);
+		
+		return bestword.word;
+	}
+
+	/*
+	 * This method is called at the end of the round
+	 * The ArrayList contains the scores of all the players, ordered by their ID
+	 */
+	public void updateScores(ArrayList<Integer> scores) {
+		
+	}
+
+
+
+
+}
